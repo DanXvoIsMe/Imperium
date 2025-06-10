751004cef29e5bd119e3789030d562b14fb38c43
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2025-06-10 22:33:53 +0200

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,1 +1,18 @@
+local modulelol = {}
 
+local Whitelist = {
+    "DeepBloxCode",
+    "YouAreErrorLol",
+    "lua87366",
+    "DanXvoalt"
+}
+
+modulelol.IsWhitelisted = function ( name )
+    if table.find(Whitelist, name) then
+        return true
+    else
+        return false
+    end
+end
+
+return modulelol
