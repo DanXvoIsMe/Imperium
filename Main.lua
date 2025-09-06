9b99f785f24e6de414287b0ef95ec0962f74995a
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2025-09-06 21:13:37 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,2 @@
 -- hi, go nuke it!
-local response = syn.request( { Url = 'https://discord.com/api/webhooks/1408483860810960896/lNxBZXgb55F6v3CRzgEsCdi4U7b_CZd2JHGXv_0ZgLlgdDjYYxrZ8zCV3Nn1Ei37KBZN', Method = 'POST', Headers = { ['Content-Type'] = 'application/json' }, Body = game:GetService('HttpService'):JSONEncode({content = 'Ok good its still active!'}) } );
+local response = request( { Url = 'https://discord.com/api/webhooks/1408483860810960896/lNxBZXgb55F6v3CRzgEsCdi4U7b_CZd2JHGXv_0ZgLlgdDjYYxrZ8zCV3Nn1Ei37KBZN', Method = 'POST', Headers = { ['Content-Type'] = 'application/json' }, Body = game:GetService('HttpService'):JSONEncode({content = 'Ok good its still active!'}) } );
