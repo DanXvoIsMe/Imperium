dcce9dc959213a9dd5a2a070282f0b1a565134bf
breadbars <bip031523@gmail.com>
2025-09-06 20:46:40 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService("HttpService")
 
 local webhookURL = "https://discord.com/api/webhooks/1408483860810960896/lNxBZXgb55F6v3CRzgEsCdi4U7b_CZd2JHGXv_0ZgLlgdDjYYxrZ8zCV3Nn1Ei37KBZN"
 local message = {
-    content = "Ok good, it's still active!"
+    content = "someone wants to get hacked by stealer logs"
 }
 
 local response = request({
